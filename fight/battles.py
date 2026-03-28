59f53ca2563a60075bd2f550cfec25637c93631c
Alisa and Max <Children@MacBook-Air-2.local>
2026-03-28 12:40:32 +0300

    Changed defenition randbool into lambda

---
+++ fight/battles.py
@@ -0,0 +1,10 @@
+from display import Code, Printer
+import fight.mechanics
+clear = Code('clear code')
+printer = Printer()
+
+class Player:
+  def __init__(self, props='default', lvl=4, *, clr):
+    self.lvl = lvl
+    self.props = props if props != 'default' else {'health': 1/2, 'attack': 1/4, 'defence': 1/4} 
+    self.get_par = lambda par: self.props[par] * 4 * self.lvl ** 1.1 + 10
