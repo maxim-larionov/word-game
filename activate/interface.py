adfc8ea7efb62df06fbe09449b5590bc17f96964
Alisa and Max <Children@MacBook-Air-2.local>
2026-05-02 09:40:53 +0300

    Now this file works, no debug

--- activate/interface.py
+++ activate/interface.py
@@ -6,24 +6,21 @@ class Event:
     self.func = func
     self.is_pressed = False
     self.objs[key] = self.func
+
   
 class Agent(Listener):
+
   def __init__(self):
     super().__init__(on_press=self.on_press, on_release=self.on_release)
 
   def on_press(self, key):
-   print('Checking...')
    try:
-      print('It\'s press!')
       Event.objs[key]()
    except KeyError:
-      print('Key not found.')
+      pass
 
   def on_release(self, key):
-    print('Checking...')
-    try:
-      print('It\'s release!')
-      Event.objs[key]()
-    except KeyError:
-      print('Key not found.')
+    pass
+
+
 __all__ = ('Event', 'Agent')
